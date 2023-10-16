3cdc13dde800485632328b2acd47d4f234165431
Rajdeep Malakar <rajdeepm.dev@gmail.com>
2023-10-16 17:28:50 +0000

    BREAKING CHANGE: Rename `text` to `render_text` and make Font optional (required for `render_text`)
    Signed-off-by: GitHub <noreply@github.com>

--- examples/async.rs
+++ examples/async.rs
@@ -16,24 +16,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await
         .expect("Could not load font");
         let mut img = SiImage::from_network_async(
-            "https://res.cloudinary.com/zype/image/upload/w_1200,h_650/CodeWithR/Template.png",
-            sirin,
+            "https://res.cloudinary.com/zype/image/upload/w_1200,h_650/CodeWithR/Template.png"
         )
         .await;
         let mut img = img
-            .text("Hello, World!", 64.0, 480.00, 254.00, None)
-            .set_font(arial)
-            .text("Finally, it freaking works!", 48.0, 480.00, 320.0, None);
-        // let mut new_img = SiImage{font: arial, image: img};
-        // // let mut file = File::create("output_1.png").expect("Could not create bytes");
-        // // file.write_all(&new_img.render_text("Tagline!!!", 48.0, 480.0, 320.0).as_bytes());
-        // let binding = new_img.render_text("Finally!!! It freaking worked!", 48.0, 480.0, 320.0);
-        // let b = binding.clone();
-        // // println!("{}", b.len())
-        let mut file = File::create("output_2.png").expect("Could not create bytes");
-        // b.write_to(&mut file, image::ImageFormat::Png);
-        // img.set_font(arial);
-        // let a = img.render_text("Finally!!! It freaking worked!", 48.0, 480.0, 320.0);
+            .render_text("Hello, World!", 64.0, 480.00, 254.00, None, sirin)
+            .render_text("Finally, it works!", 48.0, 480.00, 320.0, None, arial);
+        let mut file = File::create("output_async.png").expect("Could not create bytes");
         image::load_from_memory(&img.to_bytes())
             .expect("Could not load image")
             .write_to(&mut file, image::ImageFormat::Png);
